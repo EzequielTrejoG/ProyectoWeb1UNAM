e0553f3e790315cbd01af43db7c8fb9ebc4ff9af
Ezequiel <ezegom01@gmail.com>
2025-04-24 20:18:35 -0600

    cambios en modelo Departamento

--- modelos/Departamento.php
+++ modelos/Departamento.php
@@ -1,34 +1,57 @@
 <?php
-
+    //incluir la configuracio de la conexion
     require "../config/conexion.php";
-
+    
+    //instanciar objeto
     Class Departamento{
+        public function __construct(){ //doble _
 
-        public function __construct(){
-            
         }
-
+    
         public function insertar($descripcion){
-            $sql = "INSERT INTO departamentos(descripcion) VALUES ('$descripcion')";
+            $sql= "INSERT INTO departamentos (descripcion) values ('$descripcion')"; //query dinamico se crea cuando se ejecuta
             return ejecutarConsultaRetornaID($sql);
         }
 
         public function editar($idDepartamento, $descripcion, $fechaActualizacion, $idEmpActualiza){
-            $sql = "UPDATE departamentos SET descripcion = '$descripcion', fechaActualizacion = '$fechaActualizacion', 
-            idEmpleado = '$idEmpActualiza' WHERE idDepartamento = '$idDepartamento'";
+            $sql="UPDATE departamentos SET descripcion='$descripcion', fechaActualizacion='$fechaActualizacion', idEmpleado='$idEmpActualiza'
+            WHERE idDepartamento='$idDepartamento'";
             return ejecutarConsulta($sql);
+
         }
 
         public function desactivar($idDepartamento){
-            $sql = "UPDATE departamentos SET activo = '0' WHERE idDepartamento = '$idDepartamento'";
+            $sql="UPDATE departamentos SET activo = '0'
+            WHERE idDepartamento='$idDepartamento'";
             return ejecutarConsulta($sql);
         }
 
         public function activar($idDepartamento){
-            $sql = "UPDATE departamentos SET activo = '1' WHERE idDepartamento = '$idDepartamento'";
+            $sql="UPDATE departamentos SET activo = '1'
+            WHERE idDepartamento='$idDepartamento'";
             return ejecutarConsulta($sql);
         }
-        
+
+        public function mostrar($idDepartamento){
+            $sql="SELECT idDepartamento, descripcion, activo, fechaCreacion, fechaActualizacion,
+            idEmpActualiza FROM departamentos
+            WHERE idDepartamento='idDepartamento'";
+            return ejecutarConsultaSimpleFila($sql);
+        }
+
+        public function listar(){
+            $sql="SELECT idDepartamento, descripcion, activo, fechaCreacion, fechaActualizacion,
+            idEmpActualiza FROM departamentos";
+            return ejecutarConsulta($sql);
+        }
+
+        public function select(){
+            $sql="SELECT idDepartamento, descripcion, activo, fechaCreacion, fechaActualizacion,
+            idEmpActualiza FROM departamentos
+            WHERE activo='1'";
+            return ejecutarConsulta($sql);
+        }
+
     }
 
 ?>
