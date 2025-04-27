74545085a586c9a74e5bf7c3adbc4cd87744937c
Ezequiel <ezegom01@gmail.com>
2025-04-26 19:11:14 -0600

    Formulario Departamentos

--- vistas/departamento.php
+++ vistas/departamento.php
@@ -34,11 +34,11 @@
                         <div class="form-group col-xl-6 col-lg-6 col-md-12 col-sm-12">
                             <label for="descripcion">Nombre del departamento</label>
                             <input class="form-control" type="hidden" name="idDepartamento" id="idDepartamento">
-                            <input class="form-control" type="text" name="descripcion" id="descripcion">
+                            <input class="form-control" type="text" name="descripcion" id="descripcion" maxlength="256" placeholder="Nombre departamento" required>
                         </div>
                         <div class="form-group col-xl-6 col-lg-6 col-md-12 col-sm-12">
-                           <button class="btn btn-primary">Guardar</button>
-                           <button class="btn btn-danger">Cancelar</button> 
+                           <button class="btn btn-primary" id="btnGuardar" type="submit">Guardar</button>
+                           <button class="btn btn-danger" id="btnCancelar" type="clear">Cancelar</button> 
                         </div>
                     </form>
                 </div>
