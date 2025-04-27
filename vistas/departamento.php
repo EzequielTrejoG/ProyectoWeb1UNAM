44de8fb75f6aa01ff11d622fb9fb84b0e476ab58
Ezequiel <ezegom01@gmail.com>
2025-04-26 19:30:10 -0600

    Se creó la tabla de listado de departamentos

--- vistas/departamento.php
+++ vistas/departamento.php
@@ -3,23 +3,6 @@
 ?>
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Legacy User Menu</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Legacy User Menu</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
-
     <!-- Main content -->
     <section class="content">
 
@@ -28,6 +11,28 @@
             <div class="card-header">
                 <h3 class="card-title">Title</h3>
             </div>
+            <!-- Contenedor de listados-->
+            <div class="panel-body" id="listadoregdata">
+                <table id="tblistadoregdata" class="table table-striped table-condensed table-over">
+                    <thead>
+                        <th>Descripción</th>
+                        <th>Fecha de Creación</th>
+                        <th>Fecha de Actualización</th>
+                        <th>Estatus</th>
+                        <th>Empleado modificó</th>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                    <thead>
+                        <th>Descripción</th>
+                        <th>Fecha de Creación</th>
+                        <th>Fecha de Actualización</th>
+                        <th>Estatus</th>
+                        <th>Empleado modificó</th>
+                    </thead>
+                </table>
+            </div>
+            <!-- Contenedor de formulario de registro de departamento --> 
             <div class="card-body">
                 <div class="panel-body" id="formregdata">
                     <form name="formulario" id="formulario" method="POST">
@@ -44,10 +49,6 @@
                 </div>
             </div>
             <!-- /.card-body -->
-            <div class="card-footer">
-                Footer
-            </div>
-            <!-- /.card-footer-->
         </div>
         <!-- /.card -->
 
