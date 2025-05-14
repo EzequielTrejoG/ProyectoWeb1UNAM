09f1d2f1f5f61ff802493d05be68377c58371378
Ezequiel <ezegom01@gmail.com>
2025-05-14 13:17:11 -0600

    Agregué href a categorias

--- vistas/cabecero.php
+++ vistas/cabecero.php
@@ -100,7 +100,7 @@ if ($_SERVER['HTTPS'] != 'on') {
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="#" class="nav-link">
+                                    <a href="categorias" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Catálogo Categorías</p>
                                     </a>
