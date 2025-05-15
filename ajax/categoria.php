87f04f8aef2c803ef1a6e7bf950ea6b2b5ec785a
Ezequiel <ezegom01@gmail.com>
2025-05-14 18:41:40 -0600

    Ajax(Controlador) Categoría

--- ajax/categoria.php
+++ ajax/categoria.php
@@ -1,20 +1,20 @@
 <?php
 
-    require_once '../modelos/departamento.php';
+    require_once '../modelos/categoria.php';
 
     date_default_timezone_set("America/Mexico_City");
 
-    $idDepartamento = isset($_POST['idDepartamento']) ? limpiarCadenas($_POST['idDepartamento']): "";
+    $idCategoria = isset($_POST['idCategoria']) ? limpiarCadenas($_POST['idCategoria']): "";
     $descripcion = isset($_POST['descripcion']) ? limpiarCadenas($_POST['descripcion']): "";
     $fechaActualizacion = date("Y-m-d H:i:s"); 
     $idEmpActualiza = 1; //Cambiar por el usuario de la sesión 
 
-    $departamento = new Departamento();
+    $categoria = new Categoria();
 
     switch($_GET['op'])
     {
         case 'listar':
-            $rspta = $departamento->listar();
+            $rspta = $categoria->listar();
             $data = Array();
             while ($reg = $rspta->fetch_object())
             {
@@ -24,13 +24,13 @@
                     '2' => $reg->fechaActualizacion,
                     '3' => ($reg->activo) ? '<span class="badge badge-success">Activado</span>' : '<span class="badge badge-danger">Desactivado</span>',
                     '4' => $reg->idEmpleado,
-                    '5' => ($reg->activo) ? '<button class="btn btn-warning btn-sm" onclick="mostrar(' . $reg->idDepartamento . ')">
+                    '5' => ($reg->activo) ? '<button class="btn btn-warning btn-sm" onclick="mostrar(' . $reg->idCategoria . ')">
                     <i class="far fa-edit"></i></button>' .
-                    ' <button class="btn btn-danger btn-sm" title="Desactivar" onclick="desactivar(' . $reg->idDepartamento . ')">
+                    ' <button class="btn btn-danger btn-sm" title="Desactivar" onclick="desactivar(' . $reg->idCategoria . ')">
                     <i class="far fa-window-close"></i></button>' : 
-                    '<button class="btn btn-warning btn-sm" onclick="mostrar(' . $reg->idDepartamento . ')">
+                    '<button class="btn btn-warning btn-sm" onclick="mostrar(' . $reg->idCategoria . ')">
                     <i class="far fa-edit"></i></button>' .
-                    ' <button class="btn btn-primary btn-sm" title="Activar" onclick="activar(' . $reg->idDepartamento . ')">
+                    ' <button class="btn btn-primary btn-sm" title="Activar" onclick="activar(' . $reg->idCategoria . ')">
                     <i class="far fa-check-square"></i></button>'
                 );   
             }
@@ -44,31 +44,31 @@
         break;
 
         case 'guardaryeditar':
-            if(empty($idDepartamento))
+            if(empty($idCategoria))
             {
-                $rspta = $departamento->insertar($descripcion);
-                echo $rspta != 0 ? "Departamento registrado" : "Error departamento no registrado";
+                $rspta = $categoria->insertar($descripcion);
+                echo $rspta != 0 ? "Categoría registrada" : "Error categoría no registrada";
             }
             else
             {
-                $rspta = $departamento->editar($idDepartamento, $descripcion, $fechaActualizacion, $idEmpActualiza);
-                echo $rspta != 0 ? "Departamento actualizado" : "Error departamento no actualizado";
+                $rspta = $categoria->editar($idCategoria, $descripcion, $fechaActualizacion, $idEmpActualiza);
+                echo $rspta != 0 ? "Categoría actualizada" : "Error categoría no actualizada";
             }
         break;
 
         case 'mostrar':
-            $rspta = $departamento->mostrar($idDepartamento);
+            $rspta = $categoria->mostrar($idCategoria);
             echo json_encode($rspta);
         break;
 
         case 'desactivar':
-            $rspta = $departamento->desactivar($idDepartamento);
-            echo $rspta ? "Departamento desactivado" : "Error el Depto no se pudo desactivar";
+            $rspta = $categoria->desactivar($idCategoria);
+            echo $rspta ? "Categoría desactivada" : "Error la categoría no se pudo desactivar";
         break;
 
         case 'activar':
-            $rspta = $departamento->activar($idDepartamento);
-            echo $rspta ? "El departamento ha sido activado" : "Error el Depto no se pudo activar";
+            $rspta = $categoria->activar($idCategoria);
+            echo $rspta ? "La categoría ha sido activada" : "Error la categoría no se pudo activar";
         break;
 
     }
